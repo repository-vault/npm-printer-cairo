d7e5414920b5a323da770b361cbbba0000daabc2
Florent Kolosowski <fkolosowski@ivsweb.com>
2015-11-06 18:39:49 +0100

    tests (need work on error... never catch on a buffer)

--- index.js
+++ index.js
@@ -6,55 +6,27 @@ var fs = require('fs');
 var util = require('util');
 var path = require('path');
 
-
-function print(pdf_file_path, printer_name, chain) {
-
-  var printer = cp.spawn(FOXIT_BIN,  ["/t", "pan-apps/printer/tmp/tmpfile.html.pdf",  printer_name] );
-
-  printer.on('close', function (code) {
-    if(code !== 0)
-      return chain("Invalid exit code");
-    fs.unlinkSync(pdf_file_path);
-    chain();
-  });
-
-
-}
-
-
-var fs      = require('fs');
 var some    = require('mout/array/some');
 var printer = require('./lib/printer');
 
-var printers = printer.getPrinters();
+function printPDF(source_file, printerName, chain) {
+  if (!fs.existsSync(source_file))
+    return chain('unexisting file');
+  var buffer = fs.readFileSync(source_file);
+  printer.printDirect({
+    printer : printerName,
+    data: buffer,
+    type: 'RAW',
+    success: function(id) {
+      console.log('printed with id ' + id);
+      chain(null);
+    },
+    error: function(err) {
+      console.error('error on printing: ' + err);
+      return chain(err);
+    }
+  });
+}
 
-
-var printerName =  "PDFCreator";
-
-
-
-if(!some(printers, function(printer){ return printer.name== printerName; })) 
-  throw "Cannot find PDF printer (required for testing), please download one (we recommend http://azure.download.pdfforge.org/pdfcreator/2.2.1/PDFCreator-2_2_1-setup.exe";
-  
-
-
-
-var buffer = fs.readFileSync("foo.pdf");
-
-
-
-printer.printDirect({
-        printer : printerName,
-        data: buffer,
-        type: 'RAW',
-        success: function(id) {
-            console.log('printed with id ' + id);
-        },
-        error: function(err) {
-            console.error('error on printing: ' + err);
-        }
-});
-
-
-module.exports.printPDF    = print;
-module.exports.getPrinters = print;
+module.exports.printPDF    = printPDF;
+module.exports.getPrinters = printer.getPrinters;
