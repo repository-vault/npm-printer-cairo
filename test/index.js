4b08aca143be860a935cf11dfbdfd92067914910
Florent Kolosowski <fkolosowski@ivsweb.com>
2015-11-09 11:09:55 +0100

    more tests / ignore ouput tests files

--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
 "use strict";
-var expect = require('expect.js');
-var fs = require('fs');
-var printer = require('..');
-var some    = require('mout/array/some');
-var path = require('path');
+var expect = require('expect.js'),
+    fs = require('fs'),
+    printer = require('..'),
+    some    = require('mout/array/some'),
+    path = require('path'),
+    printerName = 'PDFCreator',
+    test_pdf = "foo.pdf";
 
-//var printer = require('printer');
-var printerName = '\\\\ivsfrsrv-ad1\\imprimante DEVELWEB';
-printerName = 'Foxit PhantomPDF Printer';
-var test_pdf = "foo.pdf";
-test_pdf = "Ruler_A4.pdf";
+//var printerName = '\\\\ivsfrsrv-ad1\\imprimante DEVELWEB';
+//test_pdf = "Ruler_A4.pdf";
 
 describe("Initial test suite", function(){
 
@@ -31,7 +30,6 @@ describe("Initial test suite", function(){
     }); 
   });
 
-
   it("Should print something", function(chain){
     this.timeout(30000);
     var output_path = path.join(__dirname, "incoming.pdf");
@@ -42,17 +40,60 @@ describe("Initial test suite", function(){
     var source_file = path.resolve(__dirname, test_pdf);
     printer.printPDF(source_file, printerName, function(err) {
       expect(err).not.to.be.ok();
-      
+
      var i = setInterval(function(){
         if(!fs.existsSync(output_path))
           return;
         clearInterval(i);
         chain();
       }, 200);
-    
-    }); 
+    });
   });
 
+  it ("should print a buffered pdf", function(chain) {
+    this.timeout(30000);
+    var output_path = path.join(__dirname, "incoming_buffer.pdf");
+    if(fs.existsSync(output_path))
+      fs.unlinkSync(output_path);
+
+    console.log("Make sure to save the file as '%s'", output_path);
+    var source_file = path.resolve(__dirname, test_pdf),
+        buffer = fs.readFileSync(source_file);
+    printer.printPDFBuffer(buffer, printerName, function(err) {
+      expect(err).not.to.be.ok();
+
+     var i = setInterval(function(){
+        if(!fs.existsSync(output_path))
+          return;
+        clearInterval(i);
+        chain();
+      }, 200);
+    });
+  });
+
+  it ("should print width options", function(chain) {
+    this.timeout(30000);
+    var output_path = path.join(__dirname, "incoming_options.pdf");
+    if(fs.existsSync(output_path))
+      fs.unlinkSync(output_path);
+
+    console.log("Make sure to save the file as '%s'", output_path);
+    var source_file = path.resolve(__dirname, test_pdf),
+        options = {
+          printerName : printerName,
+          orientation : 'landscape'
+        };
+    printer.printPDF(source_file, options, function(err) {
+      expect(err).not.to.be.ok();
+
+     var i = setInterval(function(){
+        if(!fs.existsSync(output_path))
+          return;
+        clearInterval(i);
+        chain();
+      }, 200);
+    });
+  });
 
 
 });
