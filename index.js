6a0f8743fc08cabcb2c7a7075b6d88ed0db7c258
Florent Kolosowski <fkolosowski@ivsweb.com>
2016-01-22 12:20:01 +0100

    convert pdf to cmyb bfore printing

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 "use strict";
 
-var cp = require('child_process');
-var fs = require('fs');
-var util = require('util');
-var path = require('path');
+var cp = require('child_process'),
+    fs = require('fs'),
+    util = require('util'),
+    path = require('path'),
+    tmppath = require('nyks/fs/tmppath');
 
-var tmppath = require('nyks/fs/tmppath');
 
-
-var gsprinter   = path.resolve(__dirname + "/lib/gsprint.exe");
-var ghostscript = path.resolve(__dirname + "/lib/gswin32c.exe");
+var gsprinter   = path.resolve(__dirname + "/lib/gsprint.exe"),
+    ghostscript = path.resolve(__dirname + "/lib/gswin32c.exe");
 
 function getPrinters(chain){
   var child = cp.spawn(gsprinter, ["-list"]),
@@ -40,7 +39,6 @@ function printPDFBuffer(buffer, options, chain) {
   });
 }
 
-
 function printPDF(source_file, options, chain) {
   if(typeof options == "string")
     options = {printer_name : options};
@@ -48,8 +46,20 @@ function printPDF(source_file, options, chain) {
   if (!fs.existsSync(source_file))
     return chain('unexisting file');
 
-  var args = ["-printer", options.printer_name, "-ghostscript", ghostscript, source_file];
-  args.push("-colour");
+  convertPDFtoCMYB(source_file, function(source_pdf) {
+    print_cmyb_pdf(source_pdf, options, chain);
+  });
+}
+
+function print_cmyb_pdf(source_file, options, chain) {
+  var args = [
+    "-printer",
+    options.printer_name,
+    "-ghostscript",
+    ghostscript,
+    source_file,
+    "-colour"
+  ];
 
   if(options.orientation) //valid are portrait / landscape
     args.push("-" + options.orientation);
@@ -70,10 +80,42 @@ function printPDF(source_file, options, chain) {
       return chain("Call to gsprint failed !");
     return chain();
   });
+}
 
-  
+function convertPDFtoCMYB(source_file, chain) {
+  var source_pdf = source_file.replace('.pdf', '_cmyb.pdf'),
+      args = [
+        "-dBATCH",
+        "-sDEVICE=pdfwrite",
+        "-dNOPAUSE",
+        "-sOutputFile=" + source_pdf,
+        "-dUseCIEColor",
+        "-sProcessColorModel=DeviceRGB",
+        "-dProcessColorModel=/DeviceCMYK",
+        "-sColorConversionStrategy=/CMYK",
+        source_file,
+        "-c",
+        "quit"
+      ],
+      child = cp.spawn(ghostscript, args),
+      body = "";
+
+  child.stdout.on("data", function(buf){
+    console.log("OUT>" + buf);
+  });
+
+  child.stderr.on("data", function(buf){
+    console.log("ERR>" + buf);
+  });
+
+  child.on("close", function(exit){
+    if(exit !== 0)
+      return chain("Call to gswin32c failed !");
+    return chain(source_pdf);
+  });
 }
 
 module.exports.printPDF         = printPDF;
 module.exports.printPDFBuffer   = printPDFBuffer;
 module.exports.getPrinters      = getPrinters;
+module.exports.convertPDFtoCMYB = convertPDFtoCMYB;
